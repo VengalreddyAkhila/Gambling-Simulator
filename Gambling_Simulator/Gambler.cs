cfba1158da69bdabe135fcce06b0e6d1bf6d0b6a
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-10 13:30:17 +0530

    Refactor:Win_Loss

--- Gambling_Simulator/Gambler.cs
+++ Gambling_Simulator/Gambler.cs
@@ -7,21 +7,20 @@ namespace Gambling_Simulator
     class Gambler
     {
        
-         const int bet = 1;
-         const int stake = 100;
+         const int BET = 1;
+         const int STAKE = 100;
         public void gamblerstake()
         {
-            Console.WriteLine("stake" + stake + "bet" + bet);
+            Console.WriteLine("stake" + STAKE + "bet" + BET);
             Random random = new Random();
             int stakeholder = random.Next(0, 2);
-
-            for (int i = 0; i <= stake; i++)
-            {
-                if (stakeholder  == 1)
+            
+                if (stakeholder == 1)
                     Console.WriteLine("wins");
                 else
                     Console.WriteLine("loss");
-            }
+            
+
         }
 
         
