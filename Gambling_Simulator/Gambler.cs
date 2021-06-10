04e807ac8e7104a596c8d6ece0a145080ddbe4b7
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-10 12:45:50 +0530

    Refactor:UC1-Stakeholder

---
+++ Gambling_Simulator/Gambler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gambling_Simulator
+{
+    class Gambler
+    {
+       
+        int bet = 1;
+        int stake = 100;
+        public void gamblerstake()
+        {
+
+            for (int i = 0; i <= stake; i++)
+            {
+                if (bet == 1)
+                    Console.WriteLine("win");
+                else
+                    Console.WriteLine("loss");
+
+
+            }
+        }
+
+        
+           
+    }
+}
---
+++ Gambling_Simulator/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+namespace Gambling_Simulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Gambling_Simulator_Game");
+            Gambler gambler = new Gambler();
+            gambler.gamblerstake();
+        }
+    }
+}
